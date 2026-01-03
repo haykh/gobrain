43c0501d28874995c9fd0334910d473e3fd6a261
hayk <haykh.astro@gmail.com>
2026-01-03 02:09:05 -0500

    more templates for testing

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.gobrain/tasks/ &&
-  cp -r template/tasks ~/.gobrain/tasks &&
-  go run .
+rm -rf /tmp/.gobrain/ &&
+  mkdir -p /tmp/.gobrain/ &&
+  cp -r template/tasks /tmp/.gobrain/tasks &&
+  cp -r template/random_notes /tmp/.gobrain/random_notes &&
+  cp -r template/daily_notes /tmp/.gobrain/daily_notes &&
+  go build . &&
+  ./gobrain -k -d -h /tmp/.gobrain
