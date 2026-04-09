37340df7c93aeaf7142a535f75477add4fcb79d3
haykh <haykh.astro@gmail.com>
2026-04-08 20:42:39 -0400

    flake upd

--- flake.nix
+++ flake.nix
@@ -2,46 +2,57 @@
   description = "a terminal-based notes and tasks organizer";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
     {
-      packages.${system}.default = pkgs.buildGoModule rec {
-        name = "gobrain";
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { stdenv.hostPlatform.system = system; };
+
+        pname = "gobrain";
+        version = "1.4.0";
         author = "haykh";
 
         src = pkgs.fetchFromGitHub {
+          inherit pname version;
           owner = author;
-          repo = name;
+          repo = pname;
           rev = "master";
           hash = "sha256-KGGgXZgy6Un/YW22Kbl39PMGyY2j7PmxhFPjCY24iyY=";
         };
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          inherit pname version src;
 
-        vendorHash = "sha256-AfdgJceYwgZB0lVpVRWYoGkbBQWWCV+FHx33Ru7lCUM=";
+          vendorHash = "sha256-AfdgJceYwgZB0lVpVRWYoGkbBQWWCV+FHx33Ru7lCUM=";
 
-        meta = with pkgs.lib; {
-          homepage = "https://github.com/${author}/gobrain";
-          license = licenses.unlicense;
-          maintainers = [ author ];
+          meta = with pkgs.lib; {
+            homepage = "https://github.com/${author}/gobrain";
+            license = licenses.unlicense;
+            maintainers = [ author ];
+          };
         };
-      };
 
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.go
-          pkgs.gopls
-          pkgs.gotools
-        ];
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.gopls
+            pkgs.gotools
+          ];
 
-        shellHook = ''
-          echo "Welcome to gobrain development shell!"
-        '';
-      };
-    };
+          shellHook = ''
+            echo "Welcome to gobrain development shell!"
+          '';
+        };
+      }
+    );
 }
